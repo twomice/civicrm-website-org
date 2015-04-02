743a809c60d1f8e4d9857384e7470c623c725344
kurund <kurund@civicrm.org>
2015-04-02 15:52:25 +0530

    added download buttton instead of redirect

--- drupal/sites/all/modules/cividownload/cividownload-redirect.js
+++ drupal/sites/all/modules/cividownload/cividownload-redirect.js
@@ -1,10 +1,6 @@
 (function ($) {
   $(function () {
-    window.setTimeout(function(){
-      document.write("You will be redirected to download page in few seconds"); 
-      // redirection url
-      window.location.href = "/download/list";
-    }, 6000);
+    $('#thankyou_text').prepend("<div style='background-color:#7EC757;padding: 11px; border: 2px solid black;text-align: center;border-radius: 12px;width: 279px;font-size: 17px;'><a style='color:#ffffff' href='/download/list'>Click here to continue with the download</a></div>");
   });
 }(jQuery));
 
